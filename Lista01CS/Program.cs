9543d187c41b105023df47a2a6afbb1ef2eda0b5
Luuiiz ツ <97256376+luuiizf@users.noreply.github.com>
2024-07-03 17:20:35 +0000

    questão 1

---
+++ Lista01CS/Program.cs
@@ -0,0 +1,7 @@
+﻿Circulo circulo = new Circulo();
+Console.WriteLine("Informe o valor do raio do circulo: ");
+circulo.raio = double.Parse(Console.ReadLine());
+Console.WriteLine("Essa é a área do círculo: ");
+Console.WriteLine(circulo.CalcArea());
+Console.WriteLine("Essa é a circunferência do círculo: ");
+Console.WriteLine(circulo.CalcCirc());
---
+++ Lista01CS/exercicio01.cs
@@ -0,0 +1,10 @@
+class Circulo {
+    public double raio;
+    public double CalcArea(){
+        return Math.PI * (raio * raio);
+    }
+    public double CalcCirc()
+    {
+        return 2 * Math.PI * raio;
+    }
+}
